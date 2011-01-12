33f4e37955bcca73e75d63641e74cf701b9efc90
mweirauch <dev@m-phasis.de>
2011-01-12 22:48:12 +0100

    msm: proc_comm_wince: added from .27

---
+++ arch/arm/mach-msm/proc_comm_wince.c
@@ -0,0 +1,220 @@
+/* arch/arm/mach-msm/proc_comm_wince.c
+ *
+ * Author: maejrep
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Based on proc_comm.c by Brian Swetland
+ */
+
+#include <linux/delay.h>
+#include <linux/errno.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/gpio.h>
+#include <linux/spinlock.h>
+#include <mach/msm_iomap.h>
+#include <mach/system.h>
+
+#include "proc_comm_wince.h"
+
+#define MSM_A2M_INT(n) (MSM_CSR_BASE + 0x400 + (n) * 4)
+
+static inline void notify_other_proc_comm(void)
+{
+	writel(1, MSM_A2M_INT(6));
+}
+
+#define PC_DEBUG 0
+
+#define PC_COMMAND      0x00
+#define PC_STATUS       0x04
+#define PC_SERIAL       0x08
+#define PC_SERIAL_CHECK 0x0C
+#define PC_DATA         0x20
+#define PC_DATA_RESULT  0x24
+
+#if (PC_DEBUG > 0)
+ #define DDEX(fmt, arg...) printk(KERN_DEBUG "[DEX] %s: " fmt "\n", __FUNCTION__, ## arg)
+#else
+ #define DDEX(fmt, arg...) do {} while (0)
+#endif
+
+
+static DEFINE_SPINLOCK(proc_comm_lock);
+
+/* The higher level SMD support will install this to
+ * provide a way to check for and handle modem restart?
+ */
+int (*msm_check_for_modem_crash)(void);
+
+#define TIMEOUT (10000000) /* 10s in microseconds */
+
+int msm_proc_comm_wince(struct msm_dex_command * in, unsigned *out)
+{
+#if !defined(CONFIG_MSM_AMSS_VERSION_WINCE)
+  #warning NON-WinCE compatible AMSS version selected. WinCE proc_comm implementation is disabled and stubbed to return -EIO.
+        return -EIO;
+#else
+	unsigned base = (unsigned)(MSM_SHARED_RAM_BASE + 0xfc100);
+	unsigned long flags;
+	unsigned timeout;
+	unsigned status;
+	unsigned num;
+	unsigned base_cmd, base_status;
+
+	spin_lock_irqsave(&proc_comm_lock, flags);
+
+	DDEX("waiting for modem; command=0x%02x data=0x%x", in->cmd, in->data);
+
+	// Store original cmd byte
+	base_cmd = in->cmd & 0xff;
+
+	// Write only lowest byte
+	writeb(base_cmd, base + PC_COMMAND);
+
+	// If we have data to pass, add 0x100 bit and store the data
+	if ( in->has_data )
+	{
+		writel(readl(base + PC_COMMAND) | DEX_HAS_DATA, base + PC_COMMAND);
+		writel(in->data, base + PC_DATA);
+	} else {
+		writel(readl(base + PC_COMMAND) & ~DEX_HAS_DATA, base + PC_COMMAND);
+		writel(0, base + PC_DATA);
+	}
+	
+	// Increment last serial counter
+	num = readl(base + PC_SERIAL) + 1;
+	writel(num, base + PC_SERIAL);
+
+	DDEX("command and data sent (cntr=0x%x) ...", num);
+
+	// Notify ARM9 with int6
+	notify_other_proc_comm();
+
+	// Wait for response...  XXX: check irq stat?
+	timeout = TIMEOUT;
+	while ( --timeout && readl(base + PC_SERIAL_CHECK) != num )
+		udelay(1);
+
+	if ( ! timeout )
+	{
+		printk(KERN_WARNING "%s: DEX cmd timed out. status=0x%x, A2Mcntr=%x, M2Acntr=%x\n", 
+			__func__, readl(base + PC_STATUS), num, readl(base + PC_SERIAL_CHECK));
+		goto end;
+	}
+	
+	DDEX("command result status = 0x%08x", readl(base + PC_STATUS));
+
+	// Read status of command
+	status = readl(base + PC_STATUS);
+	writeb(0, base + PC_STATUS);
+	base_status = status & 0xff;
+	DDEX("status new = 0x%x; status base = 0x%x", 
+		readl(base + PC_STATUS), base_status);
+
+
+	if ( base_status == base_cmd )
+	{
+		if ( status & DEX_STATUS_FAIL )
+		{
+			DDEX("DEX cmd failed; status=%x, result=%x",
+				readl(base + PC_STATUS),
+				readl(base + PC_DATA_RESULT));
+
+			writel(readl(base + PC_STATUS) & ~DEX_STATUS_FAIL, base + PC_STATUS);
+		}
+		else if ( status & DEX_HAS_DATA )
+		{
+			writel(readl(base + PC_STATUS) & ~DEX_HAS_DATA, base + PC_STATUS);
+			if (out)
+				*out = readl(base + PC_DATA_RESULT);
+			DDEX("DEX output data = 0x%x", 
+				readl(base + PC_DATA_RESULT));
+		}
+	} else {
+		printk(KERN_WARNING "%s: DEX Code not match! a2m[0x%x], m2a[0x%x], a2m_num[0x%x], m2a_num[0x%x]\n", 
+			__func__, base_cmd, base_status, num, readl(base + PC_SERIAL_CHECK));
+	}
+
+end:
+	writel(0, base + PC_DATA_RESULT);
+	writel(0, base + PC_STATUS);
+
+	spin_unlock_irqrestore(&proc_comm_lock, flags);
+	return 0;
+#endif
+}
+
+#define PLLn_BASE(n)		(MSM_CLK_CTL_BASE + 0x300 + 28 * (n))
+#define TCX0			19200000 // Hz
+#define PLL_FREQ(l, m, n)	(TCX0 * (l) + TCX0 * (m) / (n))
+
+#define DUMP_PLL(name, base) { \
+	unsigned int mode, L, M, N, freq; \
+	mode = readl(base); \
+	L = readl(base + 0x4); \
+	M = readl(base + 0x8); \
+	N = readl(base + 0xc); \
+	freq = PLL_FREQ(L, M, N); \
+	printk(KERN_INFO "%s @ %p: MODE=%08x L=%08x M=%08x N=%08x freq=%u Hz (%u MHz)\n", \
+		name, base, mode, L, M, N, freq, freq / 1000000); \
+	}
+
+// Dump useful debug stuff
+void dump_debug_stuff(void)
+{
+	unsigned int pcb_xc;
+	char amss_ver[16];
+
+	// Dump PLL params (for debug purposes, no relation to proc_comm)
+	DUMP_PLL("PLL0", PLLn_BASE(0));
+	DUMP_PLL("PLL1", PLLn_BASE(1));
+	DUMP_PLL("PLL2", PLLn_BASE(2));
+	DUMP_PLL("PLL3", PLLn_BASE(3));
+
+	// Dump PCB XC
+	pcb_xc = readl(MSM_SHARED_RAM_BASE + 0xfc048);
+	printk(KERN_INFO "PCB XC: %08x\n", pcb_xc);
+
+	// Dump AMMS version
+	*(unsigned int *) (amss_ver + 0x0) = readl(MSM_SHARED_RAM_BASE + 0xfc030 + 0x0);
+	*(unsigned int *) (amss_ver + 0x4) = readl(MSM_SHARED_RAM_BASE + 0xfc030 + 0x4);
+	*(unsigned int *) (amss_ver + 0x8) = readl(MSM_SHARED_RAM_BASE + 0xfc030 + 0x8);
+	*(unsigned int *) (amss_ver + 0xc) = readl(MSM_SHARED_RAM_BASE + 0xfc030 + 0xc);
+	amss_ver[15] = 0;
+	printk(KERN_INFO "AMSS version: %s\n", amss_ver);
+}
+
+// Initialize PCOM registers
+int msm_proc_comm_wince_init()
+{
+#if !defined(CONFIG_MSM_AMSS_VERSION_WINCE)
+        return 0;
+#else
+	unsigned base = (unsigned)(MSM_SHARED_RAM_BASE + 0xfc100);
+	unsigned long flags;
+
+	spin_lock_irqsave(&proc_comm_lock, flags);
+
+	writel(0, base + PC_DATA);
+	writel(0, base + PC_DATA_RESULT);
+	writel(0, base + PC_SERIAL);
+	writel(0, base + PC_SERIAL_CHECK);
+	writel(0, base + PC_STATUS);
+
+	spin_unlock_irqrestore(&proc_comm_lock, flags);
+	printk(KERN_INFO "%s: WinCE PCOM initialized.\n", __func__);
+
+	dump_debug_stuff();
+
+	return 0;
+#endif
+}
---
+++ arch/arm/mach-msm/proc_comm_wince.h
@@ -0,0 +1,135 @@
+/* arch/arm/mach-msm/proc_comm.h
+ *
+ * Copyright (c) 2007 QUALCOMM Incorporated
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _ARCH_ARM_MACH_MSM_MSM_PROC_COMM_CE_H_
+#define _ARCH_ARM_MACH_MSM_MSM_PROC_COMM_CE_H_
+
+// PCOM_* research taken from http://wiki.xda-developers.com/index.php?pagename=RaphaelDEX
+// Research by cr2
+
+#define DEX_HAS_DATA    0x100
+#define DEX_STATUS_FAIL 0x200
+
+enum {
+	PCOM_PMIC_WLAN_ON = 0x2,
+	PCOM_PMIC_WLAN_OFF = 0x3,
+	PCOM_PMIC_VIBRA_ON = 0x4,
+	PCOM_PMIC_VIBRA_OFF = 0x5,
+	PCOM_PMIC_IR_ON = 0x6,
+	PCOM_PMIC_IR_OFF = 0x7,
+	PCOM_PMIC_CAM_ON = 0x8,
+	PCOM_PMIC_CAM_OFF = 0x9,
+	PCOM_PMIC_VGACAM_ON = 0xa,
+	PCOM_PMIC_VGACAM_OFF = 0xb,
+	PCOM_PMIC_SD_ON = 0xc,
+	PCOM_PMIC_SD_OFF = 0xd,
+	PCOM_PMIC_LCD_ON = 0xe,
+	PCOM_PMIC_LCD_OFF = 0xf,
+	PCOM_PMIC_MDDI_ON = 0x10,
+	PCOM_PMIC_MDDI_OFF = 0x11,
+	PCOM_PMIC_BT_ON = 0x12,
+	PCOM_PMIC_BT_OFF = 0x13,
+	PCOM_POWER_OFF = 0x14,
+	PCOM_PMIC_REG_ON = 0x15,
+	PCOM_PMIC_REG_OFF = 0x16,
+	PCOM_VIBRA_ON = 0x17,
+	PCOM_VIBRA_OFF = 0x18,
+	PCOM_SET_AUDIO_PATH = 0x19,
+	PCOM_PMIC_REG_VOLTAGE = 0x1a,
+	PCOM_SETUSB_DPLUS = 0x1b,
+	PCOM_UPDATE_AUDIO = 0x1c,
+
+	//???
+	PCOM_SET_L2_LOCK_BUS_CLK = 0x1d,
+	PCOM_ARM9_LOW_SPEED = 0x1d,
+	PCOM_REGISTER_VOCODER_PCM = 0x1e,
+	PCOM_UNREGISTER_VOCODER_PCM = 0x1f,
+	PCOM_SET_CLOCK_ON = 0x20,
+	PCOM_SET_CLOCK_OFF = 0x21,
+	PCOM_RESET_ARM9 = 0x22,
+
+	PCOM_PMIC_TVOUT_AUTO_ON = 0x25,
+	PCOM_PMIC_TVOUT_AUTO_FF = 0x26,
+
+	PCOM_LCD_STATUS = 0x30,
+	PCOM_CONFIG_MPP_PINS = 0x31,
+	PCOM_SET_CHARGER_STATUS = 0x32,
+	PCOM_TASK_REGISTER = 0x33,
+	PCOM_TASK_UNREGISTER = 0x34,
+
+	PCOM_READ_RTC = 0x81,
+	PCOM_WRITE_RTC = 0x82,
+	PCOM_SET_ALARM_RTC = 0x84,
+
+	PCOM_GET_BATTERY_DATA = 0x8a,
+	PCOM_GET_BATTERY_ID = 0x8b,
+
+	PCOM_NOTIFY_ARM9_REBOOT = 0x8e,
+
+	PCOM_GET_TX_POWER = 0x90,
+	PCOM_GET_NETWORK_BAND = 0x91,
+	PCOM_GET_GSM_TX_BAND = 0x92,
+
+	PCOM_GET_SLEEP_CLOCK = 0xa2,
+
+	PCOM_FOTA_READ = 0xa4,
+	PCOM_FOTA_WRITE = 0xa5,
+};
+
+/* Constants for PCOM_GPIO_CFG */
+
+// ??
+#define GPIO_ENABLE     0
+#define GPIO_DISABLE    1
+
+// .dir (1b)
+#define GPIO_INPUT      0
+#define GPIO_OUTPUT     1
+
+// .pull (2b)
+#define GPIO_NO_PULL    0
+#define GPIO_PULL_DOWN  1
+#define GPIO_KEEPER     2
+#define GPIO_PULL_UP    3
+
+// .drvstr (4b)
+#define GPIO_2MA        0
+#define GPIO_4MA        1
+#define GPIO_6MA        2
+#define GPIO_8MA        3
+#define GPIO_10MA       4
+#define GPIO_12MA       5
+#define GPIO_14MA       6
+#define GPIO_16MA       7
+
+struct msm_dex_command {
+	char cmd;
+	char has_data;
+	unsigned data;
+};
+
+#define DEX_GPIO_CFG(a, b, c, d, e, f) (struct msm_gpio_config){ \
+		.gpio = (a), \
+		.dir = (c), \
+		.out_op = (f), \
+		.pull = (d), \
+		.func = (b), \
+		.drvstr = (e) \
+	}
+
+int msm_proc_comm_wince(struct msm_dex_command *in, unsigned *out);
+int msm_proc_comm_wince_init(void);
+
+#endif
