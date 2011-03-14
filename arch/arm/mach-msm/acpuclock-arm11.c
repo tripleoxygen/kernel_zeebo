1b26ef45d218934f980efcd293cafb02cd924093
mweirauch <dev@m-phasis.de>
2011-03-14 23:18:08 +0100

    acpuclock-arm11: the fields are kHz not Hz...

--- arch/arm/mach-msm/acpuclock-arm11.c
+++ arch/arm/mach-msm/acpuclock-arm11.c
@@ -184,21 +184,21 @@ static int pc_pll_request(unsigned id, unsigned on)
 unsigned long acpuclk_power_collapse(void) {
 	int ret = acpuclk_get_rate();
 	ret *= 1000;
-	if (ret > drv_state.power_collapse_khz)
-		acpuclk_set_rate(drv_state.power_collapse_khz, 1);
+	if (ret > drv_state.power_collapse_khz * 1000)
+		acpuclk_set_rate(drv_state.power_collapse_khz * 1000, 1);
 	return ret;
 }
 
 unsigned long acpuclk_get_wfi_rate(void)
 {
-	return drv_state.wait_for_irq_khz;
+	return drv_state.wait_for_irq_khz * 1000;
 }
 
 unsigned long acpuclk_wait_for_irq(void) {
 	int ret = acpuclk_get_rate();
 	ret *= 1000;
-	if (ret > drv_state.wait_for_irq_khz)
-		acpuclk_set_rate(drv_state.wait_for_irq_khz, 1);
+	if (ret > drv_state.wait_for_irq_khz * 1000)
+		acpuclk_set_rate(drv_state.wait_for_irq_khz * 1000, 1);
 	return ret;
 }
 
