9c9feec733abae045c1f0a26de5dc7b463d671fc
mweirauch <dev@m-phasis.de>
2011-06-18 18:04:17 +0200

    rhod: panel: sync with .27, enable pm and other stuff
    * Dropped a bunch of commented auto-bl stuff which can be incorporated
      later again.
    * Disable vreg en/dis for all variants. (missing variant detection, yet)
    * Add pwr gpio reset in pm (seems to improve wakes)
    * Experimentally drop display on and autobl register writes from the
      init sequences as these registers are written in unblank just again.

--- arch/arm/mach-msm/board-htcrhodium-panel.c
+++ arch/arm/mach-msm/board-htcrhodium-panel.c
@@ -1,4 +1,4 @@
-/* linux/arch/arm/mach-msm/board-htcrhod-panel.c
+/* linux/arch/arm/mach-msm/board-htcrhodium-panel.c
  * Based on board-trout-panel.c by: Brian Swetland <swetland@google.com>
  * Remodelled based on board-supersonic-panel.c by: Jay Tu <jay_tu@htc.com>
  */
@@ -10,133 +10,111 @@
 #include <linux/leds.h>
 #include <linux/clk.h>
 #include <linux/err.h>
-//#include <linux/microp-klt.h>
 
 #include <asm/io.h>
 #include <asm/gpio.h>
 #include <asm/mach-types.h>
 
 #include <mach/msm_fb.h>
+#include <mach/msm_iomap.h>
 #include <mach/vreg.h>
-//#include <linux/microp-klt.h>
 
 #include "board-htcrhodium.h"
-#include "proc_comm_wince.h"
 #include "devices.h"
-
-static struct cabc_t {
-	struct led_classdev lcd_backlight;
-	struct msm_mddi_client_data *client_data;
-	struct mutex lock;
-	unsigned long status;
-} cabc;
-
-enum {
-	GATE_ON = 1 << 0,
-};
-
-int panel_id;
 
 #define REG_WAIT (0xffff)
 
-/* Panel IDs for reference
-12-13 : Sharp
-1 : Auo
-5 : Hitachi
-7 : EID
-19 : AUO
-20: EID 
-*/
-struct nov_regs {
+static struct nov_regs {
 	unsigned reg;
 	unsigned val;
 } nov_init_seq[] = {
-	/* EID es3 */
-	{0x5100, 0x00},
+	/* Auo Rhod100 */
 	{0x1100, 0x01},
-	{REG_WAIT, 0x64},
-	{0x4e00, 0x00},
-	{0x180, 0x02},
-	{0x880, 0x00},
-	{0x2080, 0x33},
-	{0x2680, 0x78},
-	{0x2880, 0x3e},
-	{0x2980, 0x04},
-	{0x2c80, 0x22},
-	{0x2e80, 0x00},
-	{0xde80, 0x02},
-	{0x3a00, 0x55},
-	{0x3500, 0x00},
-	{0x4400, 0x00},
-	{0x4401, 0x00},
-	{0x5e00, 0x00},
-	{0x6a01, 0x00},
-	{0x6a02, 0x01},
-	{0x5301, 0x10},
-	{0x5500, 0x02},
-	{0x6a17, 0x01},
-	{0x6a18, 0xff},
-	{0x2900, 0x01},
-	{0x5300, 0x2c},
-	{0x5303, 0x01},
-};
-
-struct nov_regs nov_deinit_seq[] = {
-
-	{0x2800, 0x01},
-	{0x5300, 0x28},
-	{0x5500, 0x00},
-	{0x5300, 0x08},
-	{0x5e03, 0x00},
-	{0x5300, 0x00},
-	{0x1000, 0x01},
-	{REG_WAIT, 0x5},
-};
-
-struct nov_regs nov_init_seq1[] = {
-	/* Auo es1 */
-	{0x5100, 0x00},
-	{0x1100, 0x01},
-	{REG_WAIT, 0x64},
 	{0x4e00, 0x00},
 	{0x3a00, 0x05},
 	{REG_WAIT, 0x1},
+	{0x3b00, 0x00},		// hsp/vsp high trigger
+	{0x3b02, 0x00},		// vbp 1 clk
+	{0x3b03, 0x00},		// vfp 1 clk
+	{0x3b04, 0x00},		// hbp 1 clk
+	{0x3b05, 0x00},		// hfp 1 clk
 	{0x3500, 0x02},
 	{0x4400, 0x00},
 	{0x4401, 0x00},
+	{0x5100, 0x40},
 	{0x5e00, 0x00},
 	{0x6a01, 0x00},
 	{0x6a02, 0x01},
 	{0x5301, 0x10},
 	{0x5500, 0x02},
 	{0x6a17, 0x01},
-	{0x6a18, 0xff},
-	{0x2900, 0x01},
-	{0x5300, 0x2c},
+	{0x6a18, 0x40},
+	//~ {0x2900, 0x01},
+	//~ {0x5300, 0x2c},
 	{0x5303, 0x01},
 };
 
+struct nov_regs nov_init_seq1[] = {
+	/* EID Rhod100,210,	// Table modified by WisTilt2
+	       400,500 */
+	//~ {0x2900, 0x01},		// display on
+	{0x1200, 0x01},		// partial mode
+	{0x1100, 0x01},		// sleep-out
+	{0x1300, 0x01},		// normal mode
+	{0x3500, 0x00},		// tearing effect
+	{0x3a00, 0x55},		// bits per pixel
+	{0x4405, 0x00},		// set tear line 1st param
+	{0x4401, 0x00},		// set tear line 2nd param
+	{0x4e00, 0x00},		// set spi/i2c mode
+	{0x5100, 0x40},		// display brightness
+	{0x5301, 0x10},		// led control pins
+	{0x5302, 0x01},		// labc dimming on/off
+	{0x5303, 0x01},		// labc rising dimming style
+	{0x5304, 0x01},		// labc falling dimming style
+	{0x5500, 0x02},		// cabc enable & image type
+	{0x5e00, 0x04},		// cabc minimum brightness
+	{0x5e03, 0x05},		// labc adc & hysteresis enable
+	{0x5e04, 0x01},		// labc reference voltage 1.7v
+	{0x6a01, 0x00},		// pwm duty/freq control
+	{0x6a02, 0x01},		// pwm freq
+	{0x6a17, 0x00},		// cabc pwm force off
+	{0x6a18, 0x40},		// cabc pwm duty
+	{0x6f00, 0x00},		// clear ls lsb
+	{0x6f01, 0x00},		// clear ls msb
+	{0x6f02, 0x00},		// force cabc pwm off
+	{0x6f03, 0x00},		// force pwm duty
+	//~ {0x5300, 0x3c},		// set autobl bit during init
+};
+
 struct nov_regs nov_init_seq2[] = {
-	/* Sharp */
-	{0x5100, 0x00},
-	{0x2e80, 0x01},
-	{0x1100, 0x01},
-	{REG_WAIT, 0x64},
-	{0x4e00, 0x01},
-	{0x3a00, 0x05},
-	{0x3500, 0x00},
-	{0x4400, 0x00},
-	{0x4401, 0x00},
-	{0x5e00, 0x00},
-	{0x6a01, 0x00},
-	{0x6a02, 0x01},
-	{0x5301, 0x10},
-	{0x5500, 0x02},
-	{0x6a17, 0x01},
-	{0x6a18, 0xff},
-	{0x2900, 0x01},
-	{0x5300, 0x2c},
-	{0x5e03, 0x01},
+	/* EID Rhod300 */	// Table modified by WisTilt2
+	//~ {0x2900, 0x01},		// display on
+	{0x1200, 0x01},		// partial mode
+	{0x1100, 0x01},		// sleep-out
+	{0x1300, 0x01},		// normal mode
+	{0x3500, 0x00},		// tearing effect
+	{0x3a00, 0x55},		// bits per pixel
+	{0x4405, 0x00},		// set tear line 1st param
+	{0x4401, 0x00},		// set tear line 2nd param
+	{0x4e00, 0x00},		// set spi/i2c mode
+	{0x5100, 0x20},		// display brightness
+	{0x5301, 0x10},		// led control pins
+	{0x5302, 0x01},		// labc dimming on/off
+	{0x5303, 0x01},		// labc rising dimming style
+	{0x5304, 0x01},		// labc falling dimming style
+	{0x5500, 0x02},		// cabc enable & image type
+	{0x5e00, 0x04},		// cabc minimum brightness
+	{0x5e03, 0x05},		// labc adc & hysteresis enable
+	{0x5e04, 0x01},		// labc reference voltage 1.7v
+	{0x6a01, 0x00},		// pwm duty/freq control
+	{0x6a02, 0x01},		// pwm freq
+	{0x6a17, 0x00},		// cabc pwm force off
+	{0x6a18, 0xff},		// cabc pwm duty
+	{0x6f00, 0x00},		// clear ls lsb
+	{0x6f01, 0x00},		// clear ls msb
+	{0x6f02, 0x00},		// force cabc pwm off
+	{0x6f03, 0x00},		// force pwm duty
+	//~ {0x5300, 0x3c},		// set autobl bit during init
 };
 
 struct nov_regs nov_init_seq3[] = {
@@ -156,82 +134,24 @@ struct nov_regs nov_init_seq3[] = {
 	{0x5500, 0x02},
 	{0x6a17, 0x01},
 	{0x6a18, 0xff},
-	{0x2900, 0x01},
-	{0x5300, 0x2c},
+	//~ {0x2900, 0x01},
+	//~ {0x5300, 0x2c},
 	{0x5e03, 0x01},
 };
 
-static void suc_set_brightness(struct led_classdev *led_cdev,
-				enum led_brightness val)
-{
-	struct msm_mddi_client_data *client = cabc.client_data;
-	unsigned int shrink_br = val;
+struct nov_regs nov_deinit_seq[] = {
+	{0x2800, 0x01},		// display off
+   	{0x5300, 0x28},
+	{0x5500, 0x00},
+	{0x5300, 0x08},
+	{0x5E03, 0x00},
+	{0x5300, 0x00},
+	{0x1000, 0x01},		// sleep-in
+};
 
-	printk(KERN_DEBUG "set brightness = %d\n", val);
-	if (test_bit(GATE_ON, &cabc.status) == 0)
-		return;
-
-	if (val < 30)
-		shrink_br = 5;
-	else if ((val >= 30) && (val <= 143))
-		shrink_br = 104 * (val - 30) / 113 + 5;
-	else
-		shrink_br = 145 * (val - 144) / 111 + 110;
-	mutex_lock(&cabc.lock);
-	client->remote_write(client, 0, 0x5500);
-	client->remote_write(client, shrink_br, 0x5100);	
-	mutex_unlock(&cabc.lock);
-}
-
-static enum led_brightness
-suc_get_brightness(struct led_classdev *led_cdev)
-{
-	struct msm_mddi_client_data *client = cabc.client_data;
-	return client->remote_read(client, 0x5100);
-}
-
-#define DEFAULT_BRIGHTNESS 100
-static void suc_backlight_switch(int on)
-{
-#if 0
-	enum led_brightness val;
-	if (on) {
-		printk(KERN_DEBUG "turn on backlight\n");
-		set_bit(GATE_ON, &cabc.status);
-		val = cabc.lcd_backlight.brightness;
-
-		/* LED core uses get_brightness for default value
-		 * If the physical layer is not ready, we should
-		 * not count on it */
-		if (val == 0)
-			val = DEFAULT_BRIGHTNESS;
-		suc_set_brightness(&cabc.lcd_backlight, val);
-	} else {
-		clear_bit(GATE_ON, &cabc.status);
-		suc_set_brightness(&cabc.lcd_backlight, 0);
-	}
-#endif
-}
-
-static int suc_backlight_probe(struct platform_device *pdev)
-{
-	int err = -EIO;
-
-	mutex_init(&cabc.lock);
-	cabc.client_data = pdev->dev.platform_data;
-	cabc.lcd_backlight.name = "lcd-backlight";
-	cabc.lcd_backlight.brightness_set = suc_set_brightness;
-	cabc.lcd_backlight.brightness_get = suc_get_brightness;
-	err = led_classdev_register(&pdev->dev, &cabc.lcd_backlight);
-	if (err)
-		goto err_register_lcd_bl;
-	printk(KERN_DEBUG "%s successful\n", __func__);	
-	return 0;
-
-err_register_lcd_bl:
-	led_classdev_unregister(&cabc.lcd_backlight);
-	return err;
-}
+int panel_id;
+static struct vreg *vreg_lcd_1;	/* LCD1 */
+static struct vreg *vreg_lcd_2;	/* LCD2 */
 
 static int htcrhod_mddi_client_init(
 	struct msm_mddi_bridge_platform_data *bridge_data,
@@ -239,38 +159,71 @@ static int htcrhod_mddi_client_init(
 {
 	int i;
 	unsigned reg, val;
-	printk(KERN_DEBUG "%s\n", __func__);	
-	switch (panel_id)
+
+	printk(KERN_DEBUG "%s\n", __func__);
+
+	switch (panel_id)	//Added by WisTilt2 - Panel detect
 	{
-	case	0x14:	/*EID*/
+	case	0x14:	/* EID - Rhod100,210,400,500 */
+
 		client_data->auto_hibernate(client_data, 0);
-		for (i = 0; i < ARRAY_SIZE(nov_init_seq); i++) {
-			reg = cpu_to_le32(nov_init_seq[i].reg);
-			val = cpu_to_le32(nov_init_seq[i].val);
-			if (reg == REG_WAIT)
-			msleep(val);
-			else
-			client_data->remote_write(client_data, val, reg);
-		}
-		client_data->auto_hibernate(client_data, 1);
-		break;
-	case	0x1:
-	case	0x13:	/* AUO */
-		client_data->auto_hibernate(client_data, 0);
-		for (i = 0; i < ARRAY_SIZE(nov_init_seq1); i++) {
+		for (i = 0; i < ARRAY_SIZE(nov_init_seq1); i++)
+		{
 			reg = cpu_to_le32(nov_init_seq1[i].reg);
 			val = cpu_to_le32(nov_init_seq1[i].val);
 			if (reg == REG_WAIT)
-			msleep(val);
+				mdelay(val);
 			else
-			client_data->remote_write(client_data, val, reg);
+				client_data->remote_write(client_data, val, reg);
 		}
-		client_data->auto_hibernate(client_data, 1);
 		break;
+
+	case	0x15:	/* EID - Rhod300 */
+
+		client_data->auto_hibernate(client_data, 0);
+		for (i = 0; i < ARRAY_SIZE(nov_init_seq2); i++)
+		{
+			reg = cpu_to_le32(nov_init_seq2[i].reg);
+			val = cpu_to_le32(nov_init_seq2[i].val);
+			if (reg == REG_WAIT)
+				mdelay(val);
+			else
+				client_data->remote_write(client_data, val, reg);
+		}
+		break;
+
+	case	0x01:	/* AUO - Rhod100 */
+	case	0x07:	/* AUO - Rhod100 */
+	case	0x13:	/* AUO - Rhod100 */
+
+		client_data->auto_hibernate(client_data, 0);
+		for (i = 0; i < ARRAY_SIZE(nov_init_seq); i++)
+		{
+			reg = cpu_to_le32(nov_init_seq[i].reg);
+			val = cpu_to_le32(nov_init_seq[i].val);
+			if (reg == REG_WAIT)
+				mdelay(val);
+			else
+				client_data->remote_write(client_data, val, reg);
+		}
+		break;
+
 	default:
-		printk(KERN_WARNING "%s: FIXME! Don't know panel_id %d?\n", __func__, panel_id);
-		return 0;
+		printk(KERN_WARNING "%s: Unknown panel_id %x detected!\n", __func__, panel_id);
+		client_data->auto_hibernate(client_data, 0);
+		for (i = 0; i < ARRAY_SIZE(nov_init_seq1); i++)	//Default to EID on unknown
+		{
+			reg = cpu_to_le32(nov_init_seq1[i].reg);
+			val = cpu_to_le32(nov_init_seq1[i].val);
+			if (reg == REG_WAIT)
+				mdelay(val);
+			else
+				client_data->remote_write(client_data, val, reg);
+		}
 	}
+	client_data->auto_hibernate(client_data, 1);
+
+	printk(KERN_DEBUG "%s EXIT\n", __func__);
 
 	return 0;
 }
@@ -281,6 +234,9 @@ static int htcrhod_mddi_client_uninit(
 {
 	int i;
 	unsigned reg, val;
+
+	printk(KERN_DEBUG "%s\n", __func__);
+
 	for (i = 0; i < ARRAY_SIZE(nov_deinit_seq); i++) {
 		reg = cpu_to_le32(nov_deinit_seq[i].reg);
 		val = cpu_to_le32(nov_deinit_seq[i].val);
@@ -290,21 +246,10 @@ static int htcrhod_mddi_client_uninit(
 	return 0;
 }
 
-/* FIXME: remove after XA03 */
-static int backlight_control(int on)
-{
-
-	/* Portion needs to be written not to use microp */
-	//autobacklight_control(on);
-	return 0;
-}
-
 static int htcrhod_mddi_panel_blank(
 	struct msm_mddi_bridge_platform_data *bridge_data,
 	struct msm_mddi_client_data *client_data)
 {
-//	suc_backlight_switch(LED_OFF);
-//	backlight_control(0);
 	return 0;
 }
 
@@ -312,61 +257,56 @@ static int htcrhod_mddi_panel_unblank(
 	struct msm_mddi_bridge_platform_data *bridge_data,
 	struct msm_mddi_client_data *client_data)
 {
-	//printk(KERN_DEBUG "%s dbg_auto = %d cabc = %d\n", __func__, is_auto(), test_bit(GATE_ON, &cabc.status));
+	printk(KERN_DEBUG "%s\n", __func__);
 
-	//if (is_auto())
-	if(0)
-	{
-//		client_data->remote_write(client_data, 0x00, 0x6a17);
-//		backlight_control(1);
-		msleep(100);
-	}
-	else 
-	{
-//		client_data->remote_write(client_data, 0x01, 0x6a17);
-//		backlight_control(0);
-	}
-//	suc_backlight_switch(LED_FULL);
-	//msleep(10);
-	//backlight_control(1);
+	client_data->remote_write(client_data, 0x01, 0x2900);	// display on
+	client_data->remote_write(client_data, 0x2c, 0x5300);	// toggle autobl bit
+
+	printk(KERN_DEBUG "%s EXIT\n", __func__);
+
 	return 0;
 }
-
 
 static void htcrhod_mddi_power_client(
 	struct msm_mddi_client_data *client_data,
 	int on)
 {
-	printk(KERN_DEBUG "%s (%s)\n", __func__, on ? "on" : "off");
-	if (on) {
-//		gpio_set_value(RHOD_LCD_PWR2,1);
-//		gpio_set_value(RHOD_LCD_PWR1,1);
-/*		msleep(20);
-		gpio_set_value(0x52,1);
-		msleep(10);
-		gpio_set_value(0x52,0);
-		msleep(1);
-		gpio_set_value(0x52,1);
-		msleep(25);
-*/
-	} else {
-/*		msleep(104);
-		gpio_set_value(0x52,0);
-		msleep(25);*/
-//		gpio_set_value(RHOD_LCD_PWR1,0);
-//		gpio_set_value(RHOD_LCD_PWR2,0);
-//		msleep(3);
+	printk(KERN_DEBUG "%s(%s)\n", __func__, on ? "on" : "off");
 
+	// current vregs must only be enabled for non rhod400/500 due to
+	// MSM7600 using different vregs; otherwise radio+data will stop
+	// functioning when going into sleep
+
+	if (on) {
+		//~ vreg_enable(vreg_lcd_1);
+		//~ vreg_enable(vreg_lcd_2);
+		//~ mdelay(5);
+
+		gpio_tlmm_config(
+			GPIO_CFG(RHOD_LCD_RST, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL,
+				GPIO_CFG_2MA),
+			GPIO_CFG_ENABLE);
+		msleep(10);
+
+	} else {
+		gpio_tlmm_config(
+			GPIO_CFG(RHOD_LCD_RST, 0, GPIO_CFG_OUTPUT, GPIO_CFG_NO_PULL,
+				GPIO_CFG_2MA),
+			GPIO_CFG_DISABLE);
+		msleep(10);
+
+		//~ vreg_disable(vreg_lcd_1);
+		//~ vreg_disable(vreg_lcd_2);
 	}
 }
 
 extern struct resource resources_msm_fb[];
 
 static struct msm_mddi_bridge_platform_data novatec_client_data = {
-//	.init = htcrhod_mddi_client_init,
-//	.uninit = htcrhod_mddi_client_uninit,
-//	.blank = htcrhod_mddi_panel_blank,
-//	.unblank = htcrhod_mddi_panel_unblank,
+	.init = htcrhod_mddi_client_init,
+	.uninit = htcrhod_mddi_client_uninit,
+	.blank = htcrhod_mddi_panel_blank,
+	.unblank = htcrhod_mddi_panel_unblank,
 	.fb_data = {
 		.xres = 480,
 		.yres = 800,
@@ -381,28 +321,21 @@ static struct msm_mddi_platform_data mddi_pdata = {
 	.num_clients = 2,
 	.client_platform_data = {
 		{
+			// rhod+topa
 			.product_id = (0xb9f6 << 16 | 0x5580),
-			//.name = "mddi_c_b9f6_5582",
-			.name = "mddi_c_simple",
+			.name = "mddi_c_simple",//"mddi_c_b9f6_5582",
 			.id = 0,
 			.client_data = &novatec_client_data,
 			.clk_rate = 0,
 		},
 		{
+			// rhod+topa
 			.product_id = (0xb9f6 << 16 | 0x5582),
-			//.name = "mddi_c_b9f6_5582",
-			.name = "mddi_c_simple",
+			.name = "mddi_c_simple",//"mddi_c_b9f6_5582",
 			.id = 0,
 			.client_data = &novatec_client_data,
 			.clk_rate = 0,
 		}
-	},
-};
-
-static struct platform_driver suc_backlight_driver = {
-	.probe = suc_backlight_probe,
-	.driver = {
-		.owner = THIS_MODULE,
 	},
 };
 
@@ -412,24 +345,38 @@ int __init htcrhod_init_panel(void)
 
 	if(!machine_is_htcrhodium()) {
 		printk(KERN_INFO "%s: panel does not apply to this device, aborted\n", __func__);
-		return 0;
+		return -1;
 	}
-	//panel_id = readl(MSM_SPL_BASE+0x81034);
-	panel_id = 0x100;
-	printk(KERN_INFO "%s: Initializing panel id=%d\n", __func__, panel_id);
+
+	panel_id = readl(MSM_SPL_BASE + 0x81034);
+
+	printk(KERN_INFO "%s: Initializing panel type 0x%x\n", __func__, panel_id);
+
 	rc = platform_device_register(&msm_device_mdp);
 	if (rc)
 		return rc;
 
-	suc_backlight_driver.driver.name = "nov_cabc";
+	vreg_lcd_1 = vreg_get(0, "gp2");
+	if (IS_ERR(vreg_lcd_1))
+		return PTR_ERR(vreg_lcd_1);
 
-	rc = platform_driver_register(&suc_backlight_driver);
+	vreg_lcd_2 = vreg_get(0, "gp4");
+	if (IS_ERR(vreg_lcd_2))
+		return PTR_ERR(vreg_lcd_2);
+
+	rc = gpio_request(RHOD_LCD_VSYNC, "vsync");
+	if (rc)
+		return rc;
+	rc = gpio_direction_input(RHOD_LCD_VSYNC);
+	if (rc)
+		return rc;
+
+	rc = gpio_request(RHOD_LCD_RST, "lcdpwr");
 	if (rc)
 		return rc;
 
 	msm_device_mddi0.dev.platform_data = &mddi_pdata;
 	return platform_device_register(&msm_device_mddi0);
-
 }
 
 device_initcall(htcrhod_init_panel);
