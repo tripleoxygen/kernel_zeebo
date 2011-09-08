d224089540264ddc0d99f704a122606d00f7d444
mweirauch <dev@m-phasis.de>
2011-09-08 21:59:41 +0200

    rpc server dog/time dynamic setup (Alex)
    Need to be fixed up later as they init before amss
    values are setup.

--- arch/arm/mach-msm/rpc_server_dog_keepalive.c
+++ arch/arm/mach-msm/rpc_server_dog_keepalive.c
@@ -17,52 +17,51 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <mach/msm_rpcrouter.h>
+#include <mach/msm_smd.h>
 
-/* dog_keepalive server definitions */
-
-#define DOG_KEEPALIVE_PROG 0x30000015
-#if CONFIG_MSM_AMSS_VERSION==6210
-#define DOG_KEEPALIVE_VERS 0
-#define RPC_DOG_KEEPALIVE_BEACON 1
-#elif (CONFIG_MSM_AMSS_VERSION==6220) || (CONFIG_MSM_AMSS_VERSION==6225)
-#define DOG_KEEPALIVE_VERS 0x731fa727
-#define RPC_DOG_KEEPALIVE_BEACON 2
-#elif CONFIG_MSM_AMSS_VERSION==6350
-#define DOG_KEEPALIVE_VERS 0x00010000
-#define RPC_DOG_KEEPALIVE_BEACON 2
-#else
-#error "Unsupported AMSS version"
-#endif
-#define RPC_DOG_KEEPALIVE_NULL 0
-
-
-/* TODO: Remove server registration with _VERS when modem is upated with _COMP*/
+static uint32_t dog_prog;
+static uint32_t dog_vers;
+static uint32_t dog_beacon;
+static const uint32_t dog_null = 0;
 
 static int handle_rpc_call(struct msm_rpc_server *server,
 			   struct rpc_request_hdr *req, unsigned len)
 {
-	switch (req->procedure) {
-	case RPC_DOG_KEEPALIVE_NULL:
+	if (req->procedure == dog_null)
 		return 0;
-	case RPC_DOG_KEEPALIVE_BEACON:
+	if (req->procedure == dog_beacon) {
 		printk(KERN_INFO "DOG KEEPALIVE PING\n");
 		return 0;
-	default:
-		return -ENODEV;
 	}
+	return -ENODEV;
 }
 
-static struct msm_rpc_server rpc_server = {
-	.prog = DOG_KEEPALIVE_PROG,
-	.vers = DOG_KEEPALIVE_VERS,
-	.rpc_call = handle_rpc_call,
-};
+static struct msm_rpc_server rpc_server;
 
 static int __init rpc_server_init(void)
 {
+	if (!amss_get_num_value(AMSS_RPC_DOG_KEEPALIVE_PROG, &dog_prog)) {
+		printk(KERN_ERR "%s: failed to get AMSS_RPC_DOG_KEEPALIVE_PROG\n",
+				__func__);
+		return -1;
+	}
+
+	if (!amss_get_num_value(AMSS_RPC_DOG_KEEPALIVE_VERS, &dog_vers)) {
+		printk(KERN_ERR "%s: failed to get AMSS_RPC_DOG_KEEPALIVE_VERS\n",
+				__func__);
+		return -1;
+	}
+
+	if (!amss_get_num_value(AMSS_RPC_DOG_KEEPALIVE_BEACON, &dog_beacon)) {
+		printk(KERN_ERR "%s: failed to get AMSS_RPC_DOG_KEEPALIVE_BEACON\n",
+				__func__);
+		return -1;
+	}
 	/* Dual server registration to support backwards compatibility vers */
+	rpc_server.prog = dog_prog;
+	rpc_server.vers = dog_vers;
+	rpc_server.rpc_call = handle_rpc_call;
 	return msm_rpc_create_server(&rpc_server);
 }
-
 
 module_init(rpc_server_init);
--- arch/arm/mach-msm/rpc_server_time_remote.c
+++ arch/arm/mach-msm/rpc_server_time_remote.c
@@ -17,24 +17,14 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <mach/msm_rpcrouter.h>
+#include <mach/msm_smd.h>
 
 /* time_remote_mtoa server definitions. */
 
 #define TIME_REMOTE_MTOA_PROG 0x3000005d
-#if CONFIG_MSM_AMSS_VERSION==6210
-#define TIME_REMOTE_MTOA_VERS 0
-#elif (CONFIG_MSM_AMSS_VERSION==6220) || (CONFIG_MSM_AMSS_VERSION==6225)
-#define TIME_REMOTE_MTOA_VERS 0x9202a8e4
-#elif CONFIG_MSM_AMSS_VERSION==6350
-#define TIME_REMOTE_MTOA_VERS 0x00010000
-#elif (CONFIG_MSM_AMSS_VERSION_WINCE)
-#include <mach/amss_para.h>
-#define TIME_REMOTE_MTOA_VERS amss_get_num_value(AMSS_ID_TIME_REMOTE_MTOA_VERS)
-#else
-#error "Unknown AMSS version"
-#endif
 #define RPC_TIME_REMOTE_MTOA_NULL   0
-#define RPC_TIME_TOD_SET_APPS_BASES 2
+
+static uint32_t tod_app_bases;
 
 struct rpc_time_tod_set_apps_bases_args {
 	uint32_t tick;
@@ -44,11 +34,10 @@ struct rpc_time_tod_set_apps_bases_args {
 static int handle_rpc_call(struct msm_rpc_server *server,
 			   struct rpc_request_hdr *req, unsigned len)
 {
-	switch (req->procedure) {
-	case RPC_TIME_REMOTE_MTOA_NULL:
+	if (req->procedure == RPC_TIME_REMOTE_MTOA_NULL)
 		return 0;
 
-	case RPC_TIME_TOD_SET_APPS_BASES: {
+	if (req->procedure == tod_app_bases) {
 		struct rpc_time_tod_set_apps_bases_args *args;
 		args = (struct rpc_time_tod_set_apps_bases_args *)(req + 1);
 		args->tick = be32_to_cpu(args->tick);
@@ -59,24 +48,31 @@ static int handle_rpc_call(struct msm_rpc_server *server,
 		       args->tick, args->stamp);
 		return 0;
 	}
-	default:
-		return -ENODEV;
-	}
+
+	printk(KERN_WARNING "[TIME]: %s unhandled rpc call procedure=%08x\n", __func__, req->procedure);
+	return -ENODEV;
 }
 
 static struct msm_rpc_server rpc_server = {
 	.prog = TIME_REMOTE_MTOA_PROG,
-#if !defined(CONFIG_MSM_AMSS_VERSION_WINCE)
-	.vers = TIME_REMOTE_MTOA_VERS,
-#endif
+	.vers = 0,
 	.rpc_call = handle_rpc_call,
 };
 
 static int __init rpc_server_init(void)
 {
-#if defined(CONFIG_MSM_AMSS_VERSION_WINCE)
-	rpc_server.vers = TIME_REMOTE_MTOA_VERS;
-#endif
+	uint32_t time_vers;
+	if (!amss_get_num_value(AMSS_TIME_REMOTE_MTOA_VERS, &time_vers)) {
+		printk(KERN_ERR "%s: failed to get AMSS_TIME_REMOTE_MTOA_VERS\n",
+				__func__);
+		return -1;
+	}
+	if (!amss_get_num_value(AMSS_TIME_TOD_SET_APPS_BASES, &tod_app_bases)) {
+		printk(KERN_ERR "%s: failed to get AMSS_TIME_TOD_SET_APPS_BASES\n",
+				__func__);
+		return -1;
+	}
+	rpc_server.vers = time_vers;
 	/* Dual server registration to support backwards compatibility vers */
 	return msm_rpc_create_server(&rpc_server);
 }
